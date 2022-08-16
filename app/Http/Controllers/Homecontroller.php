218a9b64670bae2b79874f312c54929ccb383b11
siddhivaghasiya <95284637+siddhivaghasiya@users.noreply.github.com>
2022-08-16 08:35:28 -0400

    setup project

---
+++ app/Http/Controllers/Homecontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Homecontroller extends Controller
+{
+    //
+
+        public function index(){
+
+    	return view('index');
+    }
+}
---
+++ resources/views/adminlt/layout.blade.php
@@ -0,0 +1,8 @@
+@include('adminlt.common.header')
+@include('adminlt.common.sidebar')
+
+
+
+@include('adminlt.common.body')
+@include('adminlt.common.footer')
+  
